05647537239e47aced869e2f26d845e1ea846c43
Tiago Prosp <tiago@prosp>
2019-01-22 17:42:30 +0000

    final style commit

--- components/keypad.js
+++ components/keypad.js
@@ -2,14 +2,16 @@ import Button from 'preact-material-components/Button'
 
 const pad = ['1','2','3','4','5','6','7','8','9','C','0','.']
 const styles = {
-    height: '100%'
+    height: '100%',
 }
 
 const Keypad = ({click}) => {
     return (
         <div class='keypad'>
             {pad.map(v => {
-                return <Button raised style={styles} onClick={click}>{v}</Button>
+                return (<Button raised style={styles} onClick={click}>
+                    <span class='mdc-typography mdc-typography--headline4'>{v}</span>
+                </Button>)
                 }) }
         </div>
         
