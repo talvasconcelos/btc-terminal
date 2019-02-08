73ab21d0508516f4e6fd47854405225faf0beb2e
Tiago Prosp <tiago@prosp>
2019-02-08 18:24:24 +0000

    btcpay pos url modal

--- components/boardingmodal.js
+++ components/boardingmodal.js
@@ -7,7 +7,7 @@ const BoardingModal = ({open, change, click, url}) => {
         <Dialog class={open ? 'mdc-dialog--open' : ''} onAccept={click}>
             <Dialog.Header>Initial setup</Dialog.Header>
             <Dialog.Body>
-              <TextField fullwidth type='url' placeholder="BTC Pay Url" onChange={change} value={url}/>             
+              <TextField fullwidth type='url' placeholder="BTCPay PoS Url" onChange={change} value={url}/>             
             </Dialog.Body>
             <Dialog.Footer>
                 <Dialog.FooterButton accept={true} onClick={click}>Accept</Dialog.FooterButton>
